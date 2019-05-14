1939204879277221a26403805c34b2a31e93aee4
Anjuide <ange.gnamba@gmail.com>
2019-05-14 20:33:32 +0100

    Modif css liste de contact

--- profil.php
+++ profil.php
@@ -22,75 +22,84 @@
 
 <body>
 	<div class="container">
-		<!-- Titre de la page  -->
-		<h1>A PROPOS</h1>
-
 		<div class="container_details">
+			<!-- Titre de la page  -->
+			<div class="container_enTete">
+				<h1 class="enTete">À propos</h1>
+			</div>
 			<!-- BLOC D'IMAGES -->
 			<div class="container_picture">
 				<img class="picture" src="<?php echo "$personne->url_photo"; ?>" alt="Photo de profil de <?php echo "$personne->nom $personne->prenom"; ?>">
 			</div>
 			<!-- BLOC D'INFOS -->
 			<div class="container_info">
-				<p><?php echo "$personne->nom $personne->prenom"; ?></p>
-				<p><?php echo "$personne->date_naissance"; ?></p>
-				<p><?php echo "$personne->statut_couple"; ?></p>
+				<p class="info"><?php echo "$personne->prenom $personne->nom"; ?></p>
+				<p class="info"><?php echo "$personne->date_naissance"; ?></p>
+				<p class="info"><?php echo "$personne->statut_couple"; ?></p>
 			</div>
 		</div> <!-- FIN BLOC DE DETAILS -->
-		<h2>Liste de contacts</h2>
-		<div class="containerMusicHobbyPersonne">
 
-			<div class="musicHobby">
-				<h2>Loisirs</h2>
-				<div class="musique">
-					<h3>Musique</h3>
-					<ul style="list-style-type:disc">
-						<?php
-						foreach ($musiqueListe as $musique) {
-							echo "<li>$musique->type</li>";
-						};
-						?>
-					</ul>
+		<div class="container_MusicHobby">
+
+			<div class="container_music">
+				<div class="container_enTete">
+					<h2 class="enTete">Musique</h2>
 				</div>
-				<div class="hobbies">
-					<h3>Hobbies</h3>
-					<ul style="list-style-type:disc">
-						<?php
-						foreach ($hobbyListe as $hobby) {
-							echo "<li>$hobby->type</li>";
-						};
-						?>
-					</ul>
+				<ul style="list-style-type:disc">
+					<?php
+					foreach ($musiqueListe as $musique) {
+						echo "<li>$musique->type</li>";
+					};
+					?>
+				</ul>
+			</div>
+			<div class="container_hobbies">
+				<div class="container_enTete">
+					<h2 class="enTete">Hobbies</h2>
 				</div>
+				<ul style="list-style-type:disc">
+					<?php
+					foreach ($hobbyListe as $hobby) {
+						echo "<li>$hobby->type</li>";
+					};
+					?>
+				</ul>
 			</div>
 
-			<div class="container_friendslisting">
+		</div> <!-- FIN BLOC MUSIC HOBBY -->
+		<div class="container_relation">
+			<div class="container_enTete">
+				<h2 class="enTete">Liste de contacts</h2>
+			</div>
+			<div class="container_friends">
 				<?php
 				foreach ($relationListe as $relation) {
 					$id_personne = $relation->relation_id;
 					$relationInfos = $appliBD->getPersonneById($id_personne);
 					foreach ($relationInfos as $relationInfos) {
-						echo '<div class="container_friendsdetails">';
-						/* <!-- BLOC D'IMAGES --> */
-						echo '<div class="container_friendspicture">';
-						echo "<a href='profil.php?id=$relationInfos->id'>";
-						echo "<img class='friendspicture' src='$relationInfos->url_photo' alt='Photo de profil de $relationInfos->nom'>";
-						echo "</a>";
-						echo "</div>";
-						/* <!-- BLOC D'INFOS --> */
-						echo '<div class="container_friendsinfo">';
-						echo "<a class='friendsinfo' href='profil.php?id=$relationInfos->id'>";
-						echo "$relationInfos->nom $relationInfos->prenom";
-						echo "</a>";
-						echo "</div>";
-						echo "</div>"; /* <!-- FIN BLOC DE DETAILS DES PERSONNES --> */
+						?>
+						<div class="container_friend_details">
+							<?php
+							/* <!-- BLOC D'IMAGES --> */
+							echo '<div class="container_friend_picture">';
+							echo "<a href='profil.php?id=$relationInfos->id'>";
+							echo "<img class='friend_picture' src='$relationInfos->url_photo' alt='Photo de profil de $relationInfos->nom'>";
+							echo "</a>";
+							echo "</div>";
+							/* <!-- BLOC D'INFOS --> */
+							echo '<div class="container_friend_info">';
+							echo "<a class='friend_info' href='profil.php?id=$relationInfos->id'>";
+							echo "$relationInfos->nom $relationInfos->prenom";
+							echo "</a>";
+							echo "</div>";
+							?>
+						</div> <?php
+						};
 					};
-				};
-				?>
+					?>
 			</div>
 
 		</div> <!-- FIN BLOC MUSIC HOBBY PERSONNE -->
-
 	</div> <!-- FIN BLOC CONTENU -->
 </body>
 
