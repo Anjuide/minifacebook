0d252629519d63110ae8d791a0c1d2e75730cc90
Anjuide <ange.gnamba@gmail.com>
2019-05-14 18:49:14 +0100

    Modif css et formulaire

--- formulaire.php
+++ formulaire.php
@@ -8,113 +8,114 @@
 	<link rel="stylesheet" href="css/formulaire.css">
 	<link rel="icon" href="images/favicon.png">
 	<title>Inserer un nouveau profil</title>
-	
-	<?php 	
-			header('Content-Type: text/html; charset=utf-8');
-			include 'header.php';
-			require 'connexion.php';
-			$appliBD = new Connexion();
-			$appliBD->selectAllMusic();
-			$appliBD->selectAllHobby(); 
+
+	<?php
+	header('Content-Type: text/html; charset=utf-8');
+	include 'header.php';
+	require 'connexion.php';
+	$appliBD = new Connexion();
+	$appliBD->selectAllMusic();
+	$appliBD->selectAllHobby();
 	?>
 </head>
 
 <body>
-	<h1>
-		Insérer un nouveau profil
-	</h1>
-	<div class="formulaire">
-		<form method="post" action="formulaire_action.php">
-			<div class="row">
-				<div class="col-25">
-					<label for="nom">Nom:</label>
+	<div class="container">
+		<h1>
+			Insérer un nouveau profil
+		</h1>
+		<div class="formulaire">
+			<form method="post" action="formulaire_action.php">
+				<div class="row">
+					<div class="col-25">
+						<label for="nom">Nom:</label>
+					</div>
+					<div class="col-75">
+						<input type="text" name="nom" id="nom" placeholder="Veuillez entrer votre nom" required>
+					</div>
 				</div>
-				<div class="col-75">
-					<input type="text" name="nom" id="nom" placeholder="Veuillez entrer votre nom" required>
+				<div class="row">
+					<div class="col-25">
+						<label for="prenom">Prénom:</label>
+					</div>
+					<div class="col-75">
+						<input type="text" name="prenom" id="prenom" placeholder="Veuillez entrer votre prénom" required>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-25">
-					<label for="prenom">Prénom:</label>
+				<div class="row">
+					<div class="col-25">
+						<label for="date_naissance">Date de naissance:</label>
+					</div>
+					<div class="col-75">
+						<input type="date" name="date_naissance" id="date_naissance" required><br>
+					</div>
 				</div>
-				<div class="col-75">
-					<input type="text" name="prenom" id="prenom" placeholder="Veuillez entrer votre prénom" required>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-25">
-					<label for="date_naissance">Date de naissance:</label>
-				</div>
-				<div class="col-75">
-					<input type="date" name="date_naissance" id="date_naissance" required><br>
-				</div>
-			</div>
 
-			<div class="row">
-				<div class="col-25">
-					<label>Statut: </label>
+				<div class="row">
+					<div class="col-25">
+						<label>Statut: </label>
+					</div>
+					<div class="col-75">
+						<input type="radio" name="statut" id="couple" value="En couple">
+						<label for="couple">En couple</label>
+						<input type="radio" name="statut" id="celibataire" value="Célibataire">
+						<label for="celibataire">Célibataire</label>
+						<input type="radio" name="statut" id="undefined" value="Non défini">
+						<label for="undefined">Non défini</label><br>
+					</div>
 				</div>
-				<div class="col-75">
-					<input type="radio" name="statut" id="couple" value="En couple">
-					<label for="couple">En couple</label>
-					<input type="radio" name="statut" id="celibataire" value="Célibataire">
-					<label for="celibataire">Célibataire</label>
-					<input type="radio" name="statut" id="undefined" value="Non défini">
-					<label for="undefined">Non défini</label><br>
+				<div class="row">
+					<div class="col-25">
+						<label for="url_photo">Photo de profil:</label>
+					</div>
+					<div class="col-75">
+						<input type="url" name="url_photo" id="url_photo" placeholder="Veuillez entrer l'URL de la photo" required><br>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-25">
-					<label for="url_photo">Photo de profil:</label>
-				</div>
-				<div class="col-75">
-					<input type="url" name="url_photo" id="url_photo" placeholder="Veuillez entrer l'URL de la photo" required><br>
-				</div>
-			</div>
 
-			<div class="row">
-				<div class="col-25">
-					<label>Musique: </label>
-				</div>
-				<div class="col-75">
-					<?php 	
-						$allMusique = $appliBD->selectAllMusic ();
-						foreach ($allMusique as $value){  
-    						echo "<input type='checkbox' name='musique[]' id='". $value->type . $value->id ."' value='". $value->id."'>";
-    						echo "<label for='". $value->type . $value->id ."'> ". $value->type ." </label>";
+				<div class="row">
+					<div class="col-25">
+						<label>Musique: </label>
+					</div>
+					<div class="col-75">
+						<?php
+						$allMusique = $appliBD->selectAllMusic();
+						foreach ($allMusique as $value) {
+							echo "<input type='checkbox' name='musique[]' id='" . $value->type . $value->id . "' value='" . $value->id . "'>";
+							echo "<label for='" . $value->type . $value->id . "'> " . $value->type . " </label>";
 						}
-					?><br>
+						?><br>
+					</div>
 				</div>
-			</div>
 
-			<div class="row">
-				<div class="col-25">
-					<label>Hobbies: </label>
+				<div class="row">
+					<div class="col-25">
+						<label>Hobbies: </label>
+					</div>
+					<div class="col-75">
+						<?php
+						$allHobbies = $appliBD->selectAllHobby();
+						foreach ($allHobbies as $value) {
+							echo "<input type='checkbox' name='hobby[]' id='" . $value->type . $value->id . "' value='" . $value->id . "'>";
+							echo "<label for='" . $value->type . $value->id . "'> " . $value->type . " </label>";
+						} ?>
+					</div>
 				</div>
-				<div class="col-75">
-				<?php 
-					$allHobbies = $appliBD->selectAllHobby ();
-					foreach ($allHobbies as $value){  
-						echo "<input type='checkbox' name='hobby[]' id='". $value->type . $value->id ."' value='". $value->id."'>";
-						echo "<label for='". $value->type . $value->id ."'> ". $value->type ." </label>";
-					}?>
-				</div>
-			</div>
 
-			<div class="row">
-				<div class="col-25">
-					<label>Ajouter une relation: </label>
-				</div>
-				<div class="col-75">
-						<?php 
-						$allPersonne = $appliBD->selectAllPersonne ();
-						foreach ($allPersonne as $value){ 
-							echo '<div class="col-75">'; 
-								echo "<input type='checkbox' name='personne[]' id='". $value->nom . $value->id ."' value='". $value->id."'>";
-								echo "<label for='". $value->nom . $value->id ."'> ". $value->nom . " " . $value->prenom ." </label>";
+				<div class="row">
+					<div class="col-25">
+						<label>Ajouter une relation: </label>
+					</div>
+					<div class="col-75">
+						<?php
+						$allPersonne = $appliBD->selectAllPersonne();
+						foreach ($allPersonne as $value) {
+							echo '<div class="col-75">';
+							echo "<input type='checkbox' name='personne[]' id='" . $value->nom . $value->id . "' value='" . $value->id . "'>";
+							echo "<label for='" . $value->nom . $value->id . "'> " . $value->nom . " " . $value->prenom . " </label>";
 							echo "</div>";
 							echo '<div class="col-25">';
-								echo '<select id="relation" name="'.$value->id.'">
+							echo '<select id="relation" name="' . $value->id . '">
 								<option> </option>
 								<option label="famille" value="Famille">Famille</option>
 								<option label="ami" value="Ami">Ami</option>
@@ -125,14 +126,15 @@
 							echo "<br>";
 						}
 						?>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<input name="submit" id="submit" type="submit" value="Insérer le profil">
-			</div>
-		</form>
+				<div class="row">
+					<input name="submit" id="submit" type="submit" value="Insérer le profil">
+				</div>
+			</form>
+		</div>
 	</div>
-	
+
 </body>
 
 </html>
--- profil.php
+++ profil.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="fr">
 
@@ -9,17 +8,18 @@
 	<link rel="stylesheet" href="css/profil.css">
 	<link rel="icon" href="images/favicon.png">
 	<title>Affichage du profil</title>
-	<?php 	include 'header.php';
-			require 'connexion.php';
-			$appliBD = new Connexion();
-			$id = $_GET["id"];
-			$personneInfos = $appliBD->getPersonneById ($id);
-			foreach ($personneInfos as $personne){ };
-			$hobbyListe = $appliBD->getPersonneHobbyById ($id);
-			$musiqueListe = $appliBD->getPersonneMusicById ($id);
-			$relationListe = $appliBD->getPersonneRelationById ($id);				
+	<?php include 'header.php';
+	require 'connexion.php';
+	$appliBD = new Connexion();
+	$id = $_GET["id"];
+	$personneInfos = $appliBD->getPersonneById($id);
+	foreach ($personneInfos as $personne) { };
+	$hobbyListe = $appliBD->getPersonneHobbyById($id);
+	$musiqueListe = $appliBD->getPersonneMusicById($id);
+	$relationListe = $appliBD->getPersonneRelationById($id);
 	?>
 </head>
+
 <body>
 	<div class="container">
 		<!-- Titre de la page  -->
@@ -28,64 +28,64 @@
 		<div class="container_details">
 			<!-- BLOC D'IMAGES -->
 			<div class="container_picture">
-				<img class="picture" src="<?php echo "$personne->url_photo" ;?>" alt="Photo de profil de <?php echo "$personne->nom $personne->prenom" ;?>"> 
+				<img class="picture" src="<?php echo "$personne->url_photo"; ?>" alt="Photo de profil de <?php echo "$personne->nom $personne->prenom"; ?>">
 			</div>
 			<!-- BLOC D'INFOS -->
 			<div class="container_info">
-				<p><?php echo "$personne->nom $personne->prenom" ;?></p>
-				<p><?php echo "$personne->date_naissance" ;?></p>
-				<p><?php echo "$personne->statut_couple" ;?></p>
+				<p><?php echo "$personne->nom $personne->prenom"; ?></p>
+				<p><?php echo "$personne->date_naissance"; ?></p>
+				<p><?php echo "$personne->statut_couple"; ?></p>
 			</div>
 		</div> <!-- FIN BLOC DE DETAILS -->
 		<h2>Liste de contacts</h2>
 		<div class="containerMusicHobbyPersonne">
-			
+
 			<div class="musicHobby">
 				<h2>Loisirs</h2>
-				<div class="musique" >
+				<div class="musique">
 					<h3>Musique</h3>
 					<ul style="list-style-type:disc">
-						<?php 
-							foreach ($musiqueListe as $musique){ 
-								echo "<li>$musique->type</li>" ;
-							};
+						<?php
+						foreach ($musiqueListe as $musique) {
+							echo "<li>$musique->type</li>";
+						};
 						?>
 					</ul>
 				</div>
 				<div class="hobbies">
 					<h3>Hobbies</h3>
 					<ul style="list-style-type:disc">
-						<?php 
-							foreach ($hobbyListe as $hobby){ 
-								echo "<li>$hobby->type</li>" ;
-							};
+						<?php
+						foreach ($hobbyListe as $hobby) {
+							echo "<li>$hobby->type</li>";
+						};
 						?>
 					</ul>
 				</div>
 			</div>
-			
+
 			<div class="container_friendslisting">
 				<?php
-					foreach ($relationListe as $relation){
-						$id_personne=$relation->relation_id;
-						$relationInfos = $appliBD->getPersonneById ($id_personne);
-						foreach ($relationInfos as $relationInfos){
-							echo '<div class="container_friendsdetails">';
-								/* <!-- BLOC D'IMAGES --> */
-								echo '<div class="container_friendspicture">';
-									echo"<a href='profil.php?id=$relationInfos->id'>";
-										echo"<img class='friendspicture' src='$relationInfos->url_photo' alt='Photo de profil de $relationInfos->nom'>";
-									echo "</a>";
-								echo "</div>";
-								/* <!-- BLOC D'INFOS --> */
-								echo '<div class="container_friendsinfo">';
-									echo"<a class='friendsinfo' href='profil.php?id=$relationInfos->id'>";
-										echo "$relationInfos->nom $relationInfos->prenom";
-									echo "</a>";
-								echo "</div>";
-							echo "</div>"; /* <!-- FIN BLOC DE DETAILS DES PERSONNES --> */
-						};
+				foreach ($relationListe as $relation) {
+					$id_personne = $relation->relation_id;
+					$relationInfos = $appliBD->getPersonneById($id_personne);
+					foreach ($relationInfos as $relationInfos) {
+						echo '<div class="container_friendsdetails">';
+						/* <!-- BLOC D'IMAGES --> */
+						echo '<div class="container_friendspicture">';
+						echo "<a href='profil.php?id=$relationInfos->id'>";
+						echo "<img class='friendspicture' src='$relationInfos->url_photo' alt='Photo de profil de $relationInfos->nom'>";
+						echo "</a>";
+						echo "</div>";
+						/* <!-- BLOC D'INFOS --> */
+						echo '<div class="container_friendsinfo">';
+						echo "<a class='friendsinfo' href='profil.php?id=$relationInfos->id'>";
+						echo "$relationInfos->nom $relationInfos->prenom";
+						echo "</a>";
+						echo "</div>";
+						echo "</div>"; /* <!-- FIN BLOC DE DETAILS DES PERSONNES --> */
 					};
+				};
 				?>
 			</div>
 
@@ -95,4 +95,3 @@
 </body>
 
 </html>
-
